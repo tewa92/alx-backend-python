0fc36bccd47fc7f329989de6bbc002df46b4593d
tewa92 <tewachewsaleigizer@gmail.com>
2024-08-05 12:14:53 -0700

    async function

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,17 +3,15 @@
 concurrent coroutines
 """
 import asyncio
-import random
-
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n:int, max_delay: int) -> list[float]:
     """
     Write an asynchronous coroutine that takes in an integer argument
     (max_delay, with a default value of 10) named wait_random that waits
     """
     wait_times = await asyncio.gather(
-        *[wait_random(max_delay) for _ in range(n)]
+        *tuple(map(lambda _: wait_random(max_delay), range(n)))
     )
     return sorted(wait_times)
