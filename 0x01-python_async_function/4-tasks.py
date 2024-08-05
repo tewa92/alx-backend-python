6d1be97bf97f9c9f8415966807903caf586cd167
tewa92 <tewachewsaleigizer@gmail.com>
2024-08-05 13:24:12 -0700

    async function

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -8,7 +8,7 @@ import asyncio
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def measure_time(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     Display the average run time. total_time
     """
