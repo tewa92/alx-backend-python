1ec174dd581b2ab879c2a59ff29347818bd23ba2
tewa92 <tewachewsaleigizer@gmail.com>
2024-08-05 12:16:39 -0700

    async function

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,7 +6,8 @@ import asyncio
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-async def wait_n(n:int, max_delay: int) -> list[float]:
+
+async def wait_n(n: int, max_delay: int) -> list[float]:
     """
     Write an asynchronous coroutine that takes in an integer argument
     (max_delay, with a default value of 10) named wait_random that waits
