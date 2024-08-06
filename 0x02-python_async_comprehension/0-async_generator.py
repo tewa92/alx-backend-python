e57da23b1df189fa1d62029382a179d3234fd87d
tewa92 <tewachewsaleigizer@gmail.com>
2024-08-06 11:26:04 -0700

    async generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """async generator"""
 
+from typing import Generator
 import asyncio
 import random
 
-
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     '''Write an asynchronous coroutine called async_generator
     that takes no arguments.
     The coroutine will loop 10 times, each time asynchronously
